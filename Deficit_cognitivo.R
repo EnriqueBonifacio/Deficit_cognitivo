b97e8d475ada2866f2957f57d62e5ba14ccc0cc3
EnriqueBonifacio <enrique.boni99@gmail.com>
2025-07-15 15:07:07 -0500

    Avance de limpieza de datos a utilizar

---
+++ Deficit_cognitivo.R
@@ -0,0 +1,24 @@
+library(readxl)
+library(tidyverse)
+base_datos = read_xlsx("BASE.xlsx", skip = 1)
+view(base_datos)
+nueva_base %>% str()
+#Limpiando la base de datos 
+
+nueva_base <- base_datos %>%
+              select(!c(1:2)) %>%
+              rename(
+              estado_marital = "ESTADO MARITAL",
+              edad = EDAD,
+              sexo = SEXO,
+              grado_instruccion = "GRADO DE INSTRUCCIÓN",
+              infeccion_covid = "¿Tuvo infección por COVID-19 en los últimos 7 meses?",
+              hospitalizado_covid = "¿Se hospitalizo por COVID-19?",
+              enfermedad_cronica =  "Antecedente de enfermedad crónica",
+              control_enfermedad = "¿Está controlada su enfermedad?",
+              clasificacion_pfeiffer = "CLASIFICACIÓN...22",
+              clasificacion_mini = "CLASIFICACIÓN...44",
+              clasificacion_fantastico = "CLASIFICACION"
+              )
+
+view(nueva_base)
